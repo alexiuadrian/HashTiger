cdf54c95224262edc0c3130c56d73bfda3563059
Alexiu Adrian <48868394+alexiuadrian@users.noreply.github.com>
2021-03-13 15:18:40 +0200

    Added Password Check API Call

--- app/src/main/java/com/adialexiu/hashtiger/ui/main/PasswordCheck.java
+++ app/src/main/java/com/adialexiu/hashtiger/ui/main/PasswordCheck.java
@@ -1,14 +1,34 @@
 package com.adialexiu.hashtiger.ui.main;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.ColorRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Debug;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.adialexiu.hashtiger.R;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.Console;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +36,10 @@ import com.adialexiu.hashtiger.R;
  * create an instance of this fragment.
  */
 public class PasswordCheck extends Fragment {
+
+    TextView textView;
+    Button button;
+    EditText editText;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -63,4 +87,89 @@ public class PasswordCheck extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_password_check, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        textView = getView().findViewById(R.id.api_request_passwords);
+        button = getView().findViewById(R.id.check_passwords_button);
+        editText = getView().findViewById(R.id.editTextPassword);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RequestQueue queue = Volley.newRequestQueue(getContext());
+
+                String password = editText.getText().toString();
+                String passToSha1 = encryptThisString(password);
+                String url = "https://api.pwnedpasswords.com/range/" + passToSha1.substring(0, 5);
+
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                if (editText.getText().length() == 0) {
+                                    textView.setTextColor(Color.RED);
+                                    textView.setText("Please input a password");
+                                }
+                                else {
+                                    if (response.contains(passToSha1.substring(5).toUpperCase())) {
+                                        textView.setTextColor(Color.RED);
+                                        textView.setText("Your password is not secure. It has been found in " +
+                                                "a cracked passwords database.");
+                                    }
+                                    else {
+                                        textView.setTextColor(Color.GREEN);
+                                        textView.setText("Your password is secure! :)");
+                                    }
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("That didn't work!");
+                    }
+                });
+
+                queue.add(stringRequest);
+            }
+        });
+
+    }
+
+    public static String encryptThisString(String input)
+    {
+        try {
+            // getInstance() method is called with algorithm SHA-1
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+            // digest() method is called
+            // to calculate message digest of the input string
+            // returned as array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+
+            // Add preceding 0s to make it 32 bit
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            // return the HashText
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
