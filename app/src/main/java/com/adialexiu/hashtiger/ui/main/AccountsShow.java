dd1e42950ef4fa3139879b9cb2b8aa36b0a0f3c9
Alexiu Adrian <48868394+alexiuadrian@users.noreply.github.com>
2021-03-14 01:22:24 +0200

    Added Fab in AccountsShow + minor UI improvements

--- app/src/main/java/com/adialexiu/hashtiger/ui/main/AccountsShow.java
+++ app/src/main/java/com/adialexiu/hashtiger/ui/main/AccountsShow.java
@@ -2,6 +2,8 @@ package com.adialexiu.hashtiger.ui.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.adialexiu.hashtiger.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -62,5 +66,20 @@ public class AccountsShow extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_accounts_show, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FloatingActionButton fab = getView().findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
     }
 }
